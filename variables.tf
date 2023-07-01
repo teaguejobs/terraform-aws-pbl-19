96f0ffd61904536e5e0fab33e01056be4d157b7c
teaguejobs <teaguejobs@gmail.com>
2023-07-01 01:28:27 +0100

    added update variable

--- variables.tf
+++ variables.tf
@@ -8,17 +8,12 @@ variable "vpc_cidr" {
 variable "enable_dns_support" {
   default = "true"
 }
-
-
-
 variable "enable_dns_hostnames" {
   default = "true"
 }
-
 variable "enable_classiclink" {
   default = "false"
 }
-
 variable "enable_classiclink_dns_support" {
   default = "false"
 }
@@ -26,68 +21,52 @@ variable "preferred_number_of_public_subnets" {
   type        = number
   description = "number of public subnets"
 }
-
 variable "preferred_number_of_private_subnets" {
   type        = number
   description = "number of private subnets"
 }
-
 variable "name" {
   type    = string
   default = "ACS"
 }
-
 variable "tags" {
   description = "A mapping of tags to assign to all resources."
   type        = map(string)
   default     = {}
 }
-
 variable "environment" {
   type        = string
   description = "Enviroment"
 }
-
 variable "ami-bastion" {
   type        = string
   description = "AMI ID for the launch template"
 }
-
-
 variable "ami-web" {
   type        = string
   description = "AMI ID for the launch template"
 }
-
-
-
 variable "ami-nginx" {
   type        = string
   description = "AMI ID for the launch template"
 }
-
-
 variable "ami-sonar" {
   type        = string
   description = "AMI ID for the launch template"
 }
-
 variable "keypair" {
   type        = string
   description = "key pair for the instances"
 }
-
 variable "account_no" {
   type        = number
   description = "the account number"
 }
 
-
 variable "master-username" {
   type        = string
   description = "RDS admin username"
 }
-
 variable "master-password" {
   type        = string
   description = "RDS master password"
