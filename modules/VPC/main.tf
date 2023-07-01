30016df5a1eea655b6d8291576245d67b699a840
teaguejobs <teaguejobs@gmail.com>
2023-07-01 12:13:34 +0100

    modified vpc/main.tf

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -1,9 +1,4 @@
 # ------------- VPC/main.tf----------------------
-# Get list of availability zones
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block                     = var.vpc_cidr
@@ -19,7 +14,10 @@ resource "aws_vpc" "main" {
     },
   )
 }
-
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 # Create public subnets
 resource "aws_subnet" "public" {
