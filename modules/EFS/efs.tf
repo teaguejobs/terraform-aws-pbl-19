607ae819ac2433dc8376ca23a6aba70d53442147
teaguejobs <teaguejobs@gmail.com>
2023-07-05 04:37:56 +0100

    update efs.tf

--- modules/EFS/efs.tf
+++ modules/EFS/efs.tf
@@ -1,20 +1,20 @@
 # create key from key management system
 resource "aws_kms_key" "ACS-kms" {
   description = "KMS key "
-  policy      = <<EOF
-  {
-  "Version": "2012-10-17",
-  "Id": "kms-key-policy",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": { "AWS": "arn:aws:iam::${var.account_no}:user/teaguejobs" },
-      "Action": "kms:*",
-      "Resource": "*"
-    }
-  ]
-}
+#   policy      = <<EOF
+#   {
+#   "Version": "2012-10-17",
+#   "Id": "kms-key-policy",
+#   "Statement": [
+#     {
+#       "Sid": "Enable IAM User Permissions",
+#       "Effect": "Allow",
+#       "Principal": { "AWS": "arn:aws:iam::${var.account_no}:user/teaguejobs" },
+#       "Action": "kms:*",
+#       "Resource": "*"
+#     }
+#   ]
+# }
 EOF
 }
 
